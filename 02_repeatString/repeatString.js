0c6eb3a564e3df9346924d5876ff27c9c9eee42a
dao vo <gary.vo32@gmail.com>
2024-02-14 13:53:55 -0800

    finished exercise 1-7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  // return num >= 0 ? str.repeat(num) : "ERROR";
+  if(num < 0) return 'ERROR';
+  let newStr = '';
+  for(let i = 0; i < num; i++){
+    newStr += str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  console.log("test1");
+  if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
 
+  console.log("test2");
+
+  let min = num1 < num2 ? num1 : num2;
+  let max = num1 > num2 ? num1 : num2;
+  let sum = 0;
+
+  console.log("test3 ", min, max, sum);
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+    console.log("test4", sum);
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
