ee746a48fe610f37cfd32ac70ac9359ed55a7233
dao vo <gary.vo32@gmail.com>
2024-02-17 21:36:36 -0800

    finished 08 task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,curr) => total += curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => total *= curr, 1);
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+	return base ** expo;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1; 
+	return num > 0 ? factorial(num - 1) * num : num;
 };
 
 // Do not edit below this line
