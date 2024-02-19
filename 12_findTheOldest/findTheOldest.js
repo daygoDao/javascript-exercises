ab3b98821911889286824f6f96e2a9049a0088fe
dao vo <gary.vo32@gmail.com>
2024-02-18 19:44:23 -0800

    finished task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let eldest = people[0];
+  for(let person of people){
+    const personAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+    const eldestAge = (eldest.yearOfDeath || new Date().getFullYear()) - eldest.yearOfBirth;
+    if(personAge > eldestAge){
+      eldest = person;
+    }
+  }
+  return eldest;
 };
 
 // Do not edit below this line
