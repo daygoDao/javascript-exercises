0766e2f1dc8e52c2aa5db4259e72eea6f6488dd6
dao vo <gary.vo32@gmail.com>
2024-02-18 18:12:47 -0800

    finish task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) return 'OOPS'
+  if(num == 0) return 0;
+  if(num < 3) return 1;
+  let prev = 1;
+  let curr = 2;
+  for(i = 3; i < num; i++){
+    let temp = prev;
+    prev = curr;
+    curr = temp + curr;
+  }
+  return curr;
 };
 
 // Do not edit below this line
