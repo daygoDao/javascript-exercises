cfb9efd4fed3b2795c638d6c48f055367017c95a
dao vo <gary.vo32@gmail.com>
2024-02-18 17:07:52 -0800

    finished task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const regex = /[\w\d]/gi;
+  const cleanedStr = (str.match(regex)).join('').toLowerCase();
+  return cleanedStr.split('').reverse().join('') === cleanedStr;
 };
 
 // Do not edit below this line
