a50f4c2de795be40626a7cc41d9b074686408fc5
dao vo <gary.vo32@gmail.com>
2024-02-18 19:16:06 -0800

    finished task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let bookTitles = [];
+  for(let book of books){
+    // console.log(book.title);
+    bookTitles.push(book.title);
+  }
+  return bookTitles;
 };
 
 // Do not edit below this line
